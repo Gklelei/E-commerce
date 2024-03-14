663df398c9e1062ae60709e5c64ecfa1f97483a2
gklelei <leleigideon97@gmail.com>
2024-03-14 21:10:24 +0300

    Navbar done

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,28 @@
+import './Nav.css'
+import logo from '../assets/logo.png'
+import cart_icon from '../assets/cart_icon.png'
+import { useState } from 'react'
+const Nav = () => {
+    const [menu, setMenu] = useState("Shop");
+  return (
+    <div className="nav">
+<div className="nav-logo">
+    <img src={logo} alt="logo" />
+    <p>SHOPPER</p>
+</div>
+<ul className="nav-menu">
+    <li onClick={()=>{setMenu("Shop")}}>Shop {menu === "Shop" ? <hr/>:<></>}</li>
+    <li onClick={()=>{setMenu("Men")}}>Men {menu === "Men" ? <hr/>:<></>}</li>
+    <li onClick={()=>{setMenu("Women")}}>Women {menu === "Women" ? <hr/>:<></>}</li>
+    <li onClick={()=>{setMenu("Kids")}}>Kids {menu === "Kids" ? <hr/>:<></>}</li>
+</ul>
+<div className="nav-login-cart">
+    <button>Login</button>
+    <img src={cart_icon} alt="cart-icon" />
+    <div className="nav-cart-count">0</div>
+</div>
+    </div>
+  )
+}
+
+export default Nav
