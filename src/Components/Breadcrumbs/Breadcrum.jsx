c23af871dbe68660a31b4aac0d4c3f42c9db26c1
gklelei <leleigideon97@gmail.com>
2024-03-20 20:20:41 +0300

    whoops breadcrums

--- src/Components/Breadcrumbs/Breadcrum.jsx
+++ src/Components/Breadcrumbs/Breadcrum.jsx
@@ -1,10 +1,11 @@
 import './Breadcrum.css'
 import arrow_icon from '../assets/breadcrum_arrow.png'
+import { Link } from 'react-router-dom'
 const Breadcrum = (props) => {
     const { product } = props
     return (
         <div className="breadcrum">
-            HOME <img src={arrow_icon} alt="arrow" /> SHOP <img src={arrow_icon} alt="arrow" /> {product.category} <img src={arrow_icon} alt="arrow" /> {product.name}
+            <Link to={'/'}>HOME</Link><img src={arrow_icon} alt="arrow" /> <Link>SHOP</Link> <img src={arrow_icon} alt="arrow" /> <Link>{product.category} </Link> <img src={arrow_icon} alt="arrow" /> {product.name}
         </div>
     )
 }
